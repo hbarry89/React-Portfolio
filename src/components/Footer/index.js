ac67c9b26e1b20721b7a7b30bd7a17b64725a739
@hudabarry-wwMZPT <hudabarry@yahoo.com>
2023-03-30 21:59:02 -0400

    add icons

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,15 @@
 import './style.css';
 import React from 'react';
+import github from '../../assets/github.png'
+import linkedin from '../../assets/linkedin.png'
+import stackoverflow from '../../assets/stackoverflow.png'
+
 function Footer() {
   return (
     <footer>
-        <p><a href="https://github.com/hbarry89/" target="_blank" rel="noreferrer">GitHub</a> - <a href="https://www.linkedin.com/" target="_blank" rel="noreferrer">LinkedIn</a> - <a href="https://stackoverflow.com/" target="_blank" rel="noreferrer">StackOverflow</a></p>
+      <a href="https://github.com/hbarry89" target="_blank" rel="noreferrer"><img src={github} alt="GitHub Logo"></img></a>
+      <a href="https://www.linkedin.com/" target="_blank" rel="noreferrer"><img src={linkedin} alt="GitHub Logo"></img></a>
+      <a href="https://stackoverflow.com/" target="_blank" rel="noreferrer"><img src={stackoverflow} alt="GitHub Logo"></img></a>
     </footer>
   );
 }
